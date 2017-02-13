c68438c4e2a2b3a5921d9643cd1ff94e4da8545b
Andrea Baty <Andrea.f.baty@gmail.com>
2017-02-12 22:16:21 -0500

    fixing images

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -45,7 +45,7 @@ function Hero(config){
       if (enemyHealth <= 0) {
         // alert('you win!');
         $('#winning-modal').modal('show');
-        $audio.attr('src', '../images/winning-clap.mp3');
+        $audio.attr('src', './images/winning-clap.mp3');
         $audio[0].play();
         //not totally sure why this only works because it throws an error...but it works
         // clearTimeout(index.enemyHit);
@@ -76,7 +76,7 @@ function Enemy(config){
     if (heroHealth <= 0) {
       // alert('you lose!');
       $('#losing-modal').modal('show');
-      $audio.attr('src', '../images/crowd-boo.mp3');
+      $audio.attr('src', './images/crowd-boo.mp3');
       $audio[0].play();
     }else {
       return heroHealth;
@@ -91,22 +91,22 @@ Enemy.prototype = new Character();
   var luffy = new Hero({
     name: "Luffy",
     move: "Rubber Rubber Bazooka",
-    image: "../images/luffy.png",
-    thumbnail: "../images/luffy-tn.png"
+    image: "./images/luffy.png",
+    thumbnail: "./images/luffy-tn.png"
   });
 
   var robin = new Hero({
     name: "Robin",
     move: "Seis Fleur: Twist",
-    image: "../images/robin.png",
-    thumbnail: "../images/robin-tn.png"
+    image: "./images/robin.png",
+    thumbnail: "./images/robin-tn.png"
   });
 
   var zoro = new Hero({
     name: "Zoro",
     move: "Oni Cutter",
-    image: "../images/zoro.png",
-    thumbnail: "../images/zoro-tn.png"
+    image: "./images/zoro.png",
+    thumbnail: "./images/zoro-tn.png"
   });
 
 
@@ -114,22 +114,22 @@ Enemy.prototype = new Character();
   var doFlamingo = new Enemy({
     name: "Doflamingo",
     move: "Roundhouse Kick",
-    image: "../images/doflamingo.png",
-    thumbnail: "../images/doflamingo-tn.png"
+    image: "./images/doflamingo.png",
+    thumbnail: "./images/doflamingo-tn.png"
   });
 
   var smoker = new Enemy({
     name: "Arlong",
     move: "Shark Tooth Drill",
-    image: "../images/arlong.png",
-    thumbnail: "../images/arlong-tn.png"
+    image: "./images/arlong.png",
+    thumbnail: "./images/arlong-tn.png"
   });
 
   var akainu = new Enemy({
     name: "Admiral Akainu",
     move: "Volcano Meteor",
-    image: "../images/akainu.png",
-    thumbnail: "../images/akainu-tn.png"
+    image: "./images/akainu.png",
+    thumbnail: "./images/akainu-tn.png"
   });
 
   var heroes = [luffy, robin, zoro];
